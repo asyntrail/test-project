7238bcb322f201081b88e96530c09cd010e84beb
asyntrail <asyntrail@gmail.com>
2018-07-19 10:00:40 +0300

    homework 1

---
+++ src/js/slick-setting.js
@@ -0,0 +1,12 @@
+$(function() {
+$('.slider').slick({
+    // autoplay: true,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 700,
+    fade: true,
+    cssEase: 'linear'
+});
+
+});
